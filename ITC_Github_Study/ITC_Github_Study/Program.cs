c95fdde46ff4097a67d5ebcdc1f95d261c97ee6c
C2115-g-naoki <C2115@oic.jp>
2022-06-08 17:42:05 +0900

    test
    答えへコメント追加

--- ITC_Github_Study/ITC_Github_Study/Program.cs
+++ ITC_Github_Study/ITC_Github_Study/Program.cs
@@ -13,7 +13,7 @@ namespace ITC_Github_Study
             m = numInput();
 
             n += m;
-            Console.WriteLine(n);
+            Console.WriteLine("合計は{0}",n);
             Console.Read();
         }
 
